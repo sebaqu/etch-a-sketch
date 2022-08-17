a6b85b12acb24f51bbbd7e555a215d4e8c723455
Sean Baquiro <sean@sebaqu.me>
2022-08-17 00:49:24 -1000

    added new grid button

--- js/test.js
+++ js/test.js
@@ -1,7 +1,10 @@
 function createDivs(amount) {
+    const contentflex = document.querySelector('.contentflex');
+    while (contentflex.firstChild) {
+        contentflex.removeChild(contentflex.lastChild);
+    }
     if (amount >= 100) amount = 100;
     if (amount <= 0) amount = 1;
-    const contentflex = document.querySelector('.contentflex');
     for (let x = 0; x < amount; x++) {
         const row = document.createElement('div');
         row.classList.add('column');
@@ -13,16 +16,25 @@ function createDivs(amount) {
         contentflex.appendChild(row);
     }
 }
- 
-function sketchDivs() {
+
+function newGrid() {
+    const grid = prompt("Enter grid size");
+    if (grid != null && grid != '') {
+        if (!isNaN(grid)) {
+            createDivs(grid);
+            sketch();
+        } 
+        else {
+            alert(grid + " is not a number!");
+        }
+    }
+}
+
+function sketch() {
     document.querySelectorAll('.cell').forEach(cell => {
         cell.addEventListener('mouseover', e => e.target.classList.add('sketch-color'));
     });
 }
 
-function newGrid() {
-
-}
-
 createDivs(32);
-sketchDivs();
+sketch();
